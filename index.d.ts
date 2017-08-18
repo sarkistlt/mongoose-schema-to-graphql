238705299b69be0c68fd3992d097616b20aaab92
Dmitry Guzeev <dmitry.guzeev@yahoo.com>
2017-08-18 16:23:32 +0300

    Make TS typings more type-safe
    This commit makes so that it is impossible
    to pass invalid `class` field to the main
    library function.

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,17 @@ declare module 'mongoose-schema-to-graphql' {
 
     import * as mongoose from 'mongoose';
 
+    type ClassFieldType =
+        'GraphQLObjectType' |
+        'GraphQLInputObjectType' |
+        'GraphQLInterfaceType' |
+        'GraphQLUnionType' |
+        'GraphQLEnumType';
+
     type ConfigurationObject = {
         name: string,
         description?: string,
-        class: string,
+        class: ClassFieldType,
         schema: mongoose.Schema,
         exclude?: [string],
         extend?: { [key: string]: any },
