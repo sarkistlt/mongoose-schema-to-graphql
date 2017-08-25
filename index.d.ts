b1fb145f2bfd2e2729c94dd77a276a95cf817f93
Dmitry Guzeev <dmitry.guzeev@yahoo.com>
2017-08-25 15:17:40 +0300

    Fix TypeScript definitions file
    This commit makes it possible to pass a thunk
    to the `extend` option of the main library function.

--- index.d.ts
+++ index.d.ts
@@ -9,14 +9,20 @@ declare module 'mongoose-schema-to-graphql' {
         'GraphQLUnionType' |
         'GraphQLEnumType';
 
+    type ThunkType = () => { [key: string]: any };
+
+    type ObjectKeyStringValueAnyType = { [key: string]: any };
+
+    type ExtendFieldType = ObjectKeyStringValueAnyType | ThunkType;
+
     type ConfigurationObject = {
         name: string,
         description?: string,
         class: ClassFieldType,
         schema: mongoose.Schema,
         exclude?: [string],
-        extend?: { [key: string]: any },
-        fields?: { [key: string]: any },
+        extend?: ExtendFieldType,
+        fields?: ObjectKeyStringValueAnyType,
     };
 
     function mainFunction(config: ConfigurationObject): any;
